1626f108de68726b4922ebdc8f9a4fb9699b3d21
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-06-24 11:24:17 -0800

    Update Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop05 World!");
+        User user = new User("John");
+        List<UserGoal> userGoals = new List<UserGoal>();
+
+        Goal goal1 = new SimpleGoal("Complete Task 1", 10);
+        Goal goal2 = new EternalGoal("Attend Meeting", 5);
+        Goal goal3 = new ChecklistGoal("Read Books", 20, 3);
+
+        userGoals.Add(new UserGoal(user, goal1));
+        userGoals.Add(new UserGoal(user, goal2));
+        userGoals.Add(new UserGoal(user, goal3));
+
+        Console.WriteLine("User: " + user.GetUsername());
+        Console.WriteLine("Score: " + user.GetScore());
+
+        foreach (UserGoal userGoal in userGoals)
+        {
+            Goal goal = userGoal.GetGoal();
+
+            goal.DisplayGoal();
+            goal.StartActivity();
+
+            // Simulating goal completion
+            goal.CompleteGoal();
+            user.UpdateScore(goal.GetPoints());
+
+            goal.EndActivity();
+
+            Console.WriteLine("Score: " + user.GetScore());
+        }
     }
 }
