6dd9a4352d4e5ef738065252918f548ca39e2b31
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-06-24 11:33:11 -0800

    Create SimpleGoal.cs

---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,23 @@
+using System;
+
+class SimpleGoal : Goal
+{
+    public SimpleGoal(string name, int points) : base(name, points)
+    {
+    }
+
+    public override void DisplayGoal()
+    {
+        Console.WriteLine("Simple Goal: " + GetName());
+    }
+
+    public override void StartActivity()
+    {
+        Console.WriteLine("Starting simple goal activity");
+    }
+
+    public override void EndActivity()
+    {
+        Console.WriteLine("Ending simple goal activity");
+    }
+}
