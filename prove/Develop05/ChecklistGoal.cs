a5d5494aaac677f22874a8e647d11b8f8fc0fd8b
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-06-24 11:29:39 -0800

    Create ChecklistGoal.cs

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,37 @@
+using System;
+
+class ChecklistGoal : Goal
+{
+    private int _requiredCount;
+    private int _currentCount;
+
+    public ChecklistGoal(string name, int points, int requiredCount) : base(name, points)
+    {
+        _requiredCount = requiredCount;
+        _currentCount = 0;
+    }
+
+    public override void DisplayGoal()
+    {
+        Console.WriteLine("Checklist Goal: " + GetName() + " (" + _currentCount + "/" + _requiredCount + ")");
+    }
+
+    public override void StartActivity()
+    {
+        Console.WriteLine("Starting checklist goal activity");
+    }
+
+    public override void EndActivity()
+    {
+        Console.WriteLine("Ending checklist goal activity");
+    }
+
+    public void RecordGoal()
+    {
+        _currentCount++;
+        if (_currentCount == _requiredCount)
+        {
+            CompleteGoal();
+        }
+    }
+}
