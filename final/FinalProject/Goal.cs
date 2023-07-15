3aca9b51867214c4f004c92ec14ba804287f0b4b
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-07-15 12:45:51 -0800

    Goal.cs

---
+++ final/FinalProject/Goal.cs
@@ -0,0 +1,15 @@
+class Goal
+{
+    private string target;
+    private int duration;
+
+    public string getTarget()
+    {
+        return target;
+    }
+
+    public int getDuration()
+    {
+        return duration;
+    }
+}
