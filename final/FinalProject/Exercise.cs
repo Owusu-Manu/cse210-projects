56431e045967b50c52c74a87bfef851db4a20727
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-07-15 15:31:51 -0800

    Exercise.cs

--- final/FinalProject/Exercise.cs
+++ final/FinalProject/Exercise.cs
@@ -1,36 +1,11 @@
-abstract class Exercise
+using System;
+
+public abstract class Exercise
 {
-    private string name;
-    private int duration;
-    private string intensity;
+    public string Name { get; set; }
+    public TimeSpan Duration { get; set; }
+    public int Intensity { get; set; }
 
-    public string getName()
-    {
-        return name;
-    }
-
-    public int getDuration()
-    {
-        return duration;
-    }
-
-    public string getIntensity()
-    {
-        return intensity;
-    }
-
-    public virtual void start()
-    {
-        Console.WriteLine("Exercise started");
-    }
-
-    public virtual void pause()
-    {
-        Console.WriteLine("Exercise paused");
-    }
-
-    public virtual void stop()
-    {
-        Console.WriteLine("Exercise stopped");
-    }
+    public abstract void DisplayExerciseInfo();
+    public abstract double CalculateCaloriesBurned();
 }
