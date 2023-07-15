1b88c5b57a57438ea6a5e078300a604854c1021e
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-07-15 15:33:46 -0800

    Weightlifting.cs

--- final/FinalProject/Weightlifting.cs
+++ final/FinalProject/Weightlifting.cs
@@ -1,17 +1,23 @@
-class Weightlifting : Exercise
+using System;
+
+public class Weightlifting : Exercise
 {
-    public override void start()
+    public int Sets { get; set; }
+    public int Reps { get; set; }
+
+    public override void DisplayExerciseInfo()
     {
-        Console.WriteLine("Weightlifting started");
+        Console.WriteLine($"Exercise: {Name}");
+        Console.WriteLine($"Duration: {Duration.TotalMinutes} minutes");
+        Console.WriteLine($"Intensity: {Intensity}");
+        Console.WriteLine($"Sets: {Sets}");
+        Console.WriteLine($"Reps: {Reps}");
     }
 
-    public override void pause()
+    public override double CalculateCaloriesBurned()
     {
-        Console.WriteLine("Weightlifting paused");
-    }
-
-    public override void stop()
-    {
-        Console.WriteLine("Weightlifting stopped");
+        double caloriesPerMinute = 8.0 + (Intensity * 1.5);
+        double totalCaloriesBurned = caloriesPerMinute * Duration.TotalMinutes;
+        return totalCaloriesBurned;
     }
 }
