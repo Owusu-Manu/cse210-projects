a62c4538fa7ccb91e516c3fdd2f0792d33c36299
Owusu-Manu <113359591+Owusu-Manu@users.noreply.github.com>
2023-07-15 12:47:18 -0800

    Program.cs

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        // Create instances and use the FitnessTracker class
+        FitnessTracker tracker = new FitnessTracker();
+        User user = new User();
+        user.setName("John Doe");
+        tracker.addUser(user);
+
+        Exercise running = new Running();
+        Exercise weightlifting = new Weightlifting();
+        Exercise yoga = new Yoga();
+        tracker.addExercise(running);
+        tracker.addExercise(weightlifting);
+        tracker.addExercise(yoga);
+
+        Goal goal = new Goal();
+        goal.setTarget("Weight Loss");
+        goal.setDuration(30);
+        tracker.addGoal(goal);
+
+        tracker.trackExercise(running);
+
+        List<Exercise> recommendations = tracker.generateRecommendations();
+
+        tracker.displayProgress();
     }
 }
